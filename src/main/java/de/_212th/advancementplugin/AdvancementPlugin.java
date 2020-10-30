87783ce98a413a3dea9fb128737542fb30c7c233
BuildTools <unconfigured@null.spigotmc.org>
2020-10-30 20:01:42 +0100

    Added message properties support

--- src/main/java/de/_212th/advancementplugin/AdvancementPlugin.java
+++ src/main/java/de/_212th/advancementplugin/AdvancementPlugin.java
@@ -1,13 +1,23 @@
 package de._212th.advancementplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public final class AdvancementPlugin extends JavaPlugin {
+
+    private Properties messageProperties;
 
     @Override
     public void onEnable() {
 
-
+        try {
+            loadMessageProperties();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -16,6 +26,19 @@ public final class AdvancementPlugin extends JavaPlugin {
 
 
 
+    }
+
+    private void loadMessageProperties() throws IOException {
+        messageProperties = new Properties();
+        messageProperties.load(getClassLoader().getResourceAsStream("messages.properties"));
+    }
+
+    public String getColoredMessage(String key) {
+        return colorText(messageProperties.getProperty(key));
+    }
+
+    private String colorText(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
     }
 
 }
