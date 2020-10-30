78158dd52a2a0cfe382ad2dfa5406c997c50c512
BuildTools <unconfigured@null.spigotmc.org>
2020-10-30 19:58:43 +0100

    Added plugin base struct

---
+++ src/main/java/de/_212th/advancementplugin/AdvancementPlugin.java
@@ -0,0 +1,21 @@
+package de._212th.advancementplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancementPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+
+}
